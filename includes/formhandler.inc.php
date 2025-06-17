41c8442e65c57292edd891a37f04116f35a7f7c7
Shenan Vindinu <shenanvindinu@gmail.com>
2025-06-17 10:48:05 +0530

    signup form data insert prepared statement using named parameters

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -9,11 +9,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
         require_once "dbh.inc.php";
 
         $query = "INSERT INTO users (username, pwd, email) VALUES 
-        (?, ?, ?);";
+        (:username, :pwd, :email);";
 
         $stmt = $pdo->prepare($query);
 
-        $stmt->execute([$username, $pwd, $email]);
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":pwd", $pwd);
+        $stmt->bindParam(":email", $email);
+
+        $stmt->execute();
 
         $pdo = null;
         $stmt = null;
