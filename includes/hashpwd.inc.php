5c5359f703eef630f14c37dcf25cecd8005699db
Shenan Vindinu <shenanvindinu@gmail.com>
2025-06-17 15:20:13 +0530

    hashing script made to practise hashing in PHP

---
+++ includes/hashpwd.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+//Code below is a small hashing script made to practise hashing in PHP
+
+$sensitiveData = "Shenan";
+$salt = bin2hex(random_bytes(16)); // Generate random salt
+$pepper = "ASecretPepperString";
+
+$dataToHash = $sensitiveData . $salt . $pepper;
+$hash = hash("sha256", $dataToHash);
+
+/*----*/
+
+$sensitiveData = "Shenan";
+$storedSalt = $salt;
+$storedHash = $hash;
+$pepper = "ASecretPepperString";
+
+$dataToHash = $sensitiveData . $salt . $pepper;
+
+$verificationHash = hash("sha256", $dataToHash);
+
+if ($storedHash === $verificationHash) {
+    echo "The data are the same!";
+} else {
+    echo "The data are not the same";
+}
+
+//Visit this url to confirm 
+// http://localhost/website/includes/hashpwd.inc.php
