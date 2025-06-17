e6d7bf8e8aaa95ac2da7282c3ba448478538f296
Shenan Vindinu <shenanvindinu@gmail.com>
2025-06-17 12:35:46 +0530

    user search created

--- index.php
+++ index.php
@@ -9,19 +9,10 @@
 
 <body>
 
-    <h3>Change Account</h3>
-    <form action="includes/userupdate.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <input type="text" name="email" placeholder="E-Mail">
-        <button>Update</button>
-    </form>
-
-    <h3>Delete Account</h3>
-    <form action="includes/userdelete.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <button>Delete</button>
+    <form class="searchform" action="search.php" method="post">
+        <label for="search">Search for user:</label>
+        <input id="search" type="text" name="usersearch" placeholder="Search...">
+        <button>Search</button>
     </form>
 
 </body>
