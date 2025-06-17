d928bcaf5b36c9cc1a00c8c805bb29ac2a4fa9ad
Shenan Vindinu <shenanvindinu@gmail.com>
2025-06-17 13:30:19 +0530

    session start example

--- example.php
+++ example.php
@@ -1,7 +1,5 @@
 <?php
     session_start();
-
-    $_SESSION["username"] = "Shenan";
 ?>
 
 <html lang="en">
@@ -9,12 +7,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP DOC</title>
+    <title>PHP example DOC</title>
     <link rel="stylesheet" href="CSS/main.css">
 </head>
 
 <body>
 
+    <!-- our session rememmbers index pages session variable 
+     so this works as well-->
     <?php
         echo $_SESSION["username"];
     ?>
